7881ef2f26f8dcf724d6aab6d5953acc1425ecef
Remy Skelton <remy.skelton@stu.ocsb.ca>
2023-09-26 16:30:03 +0000

    Done area_per_rect_input.py

--- area_per_rect_input.py
+++ area_per_rect_input.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # Created by: Remy Skelton
-# Date: Sep. 22,2023
-# This program calculates the area and circumference of a circle
+# Date: Sep. 25,2023
+# This program calculates the area and perimeter of a rectangle with user input
 
 
 def main():
-    #User input
+    # Get User input for Length and Width
     print(
         "This find the area and perimeter of a rectangle that has your length and width calculations:"
     )
@@ -13,7 +13,7 @@ def main():
     # Declaring Variables
     length = float(input("Enter length of the rectangle (cm): "))
     width = float(input("Enter width of the rectangle (cm): "))
-    #Output
+    # Display the area and perimeter 
     print("The area is: {:.2f}cm^2".format(length * width))
     print("The perimeter is: {:.2f}cm".format(2 * (length + width)))
 
